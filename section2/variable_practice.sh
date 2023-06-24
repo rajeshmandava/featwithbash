4bbb32b3394f8b105dd19ab85ed4fb08bb63e112
rajeshmandava <rajesh.mandava07@gmail.com>
2023-06-24 23:50:16 +0200

    first go with variable in bash

---
+++ section2/variable_practice.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+employee="Sarah"
+echo "Dear ${employee}"
+echo "The board and i would like to thank you for the excellent work that you have been doing over the last few months"echo "Kepp up the good work, ${employee}"
+
+echo "Best wishes,"
+echo "Joah, CEO"
+
