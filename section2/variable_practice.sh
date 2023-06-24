7778452b389fc4fafaeef613bee68cf214e8e40f
rajeshmandava <rajesh.mandava07@gmail.com>
2023-06-24 23:56:38 +0200

    Parameter Expansion tricks in bash

--- section2/variable_practice.sh
+++ section2/variable_practice.sh
@@ -1,8 +1,14 @@
 #!/bin/bash
-employee="Sarah"
-echo "Dear ${employee}"
-echo "The board and i would like to thank you for the excellent work that you have been doing over the last few months"echo "Kepp up the good work, ${employee}"
+#employee="Sarah"
+#echo "Dear ${employee}"
+#echo "The board and i would like to thank you for the excellent work that you have been doing over the last few months"echo "Kepp up the good work, ${employee}"
 
-echo "Best wishes,"
-echo "Joah, CEO"
-
+#echo "Best wishes,"
+#echo "Joah, CEO"
+numbers=0123456789
+echo ${numbers:0:7}
+echo ${numbers:1:5}
+echo ${numbers:3}
+echo ${numbers:3:}
+echo ${numbers: -3:2}
+echo ${numbers: -3}
